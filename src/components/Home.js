10ddcad75be96448aef67fe7a7990de8330372a7
Rutvi Patel <rutvipatel2500@gmail.com>
2022-12-11 22:34:29 +0530

    added api for envelope create

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [fileURL, setFileURL] = useState();
+  const toBase64 = (file) => {
+    const reader = new FileReader();
+    file && reader.readAsDataURL(file);
+    reader.onload = () => {
+      setFileURL(reader.result);
+    };
+    reader.onerror = () => {
+      console.log('error', error);
+    };
+  };
   const logout = () => {
     const url =
       `${process.env.REACT_APP_DOCUSIGN_IDENTITY_SERVER}/logout?` +
@@ -11,11 +23,10 @@ const Home = () => {
       `redirect_uri=${encodeURIComponent(process.env.REACT_APP_URL)}&` +
       `response_mode=logout_redirect`;
     window.location = url;
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('token')
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('token');
   };
   if (window.location.hash) {
-    console.log('navigate');
     console.log('>>> url token>>>', window.location.hash);
     const regex =
       /(#access_token=)(.*)(&expires_in=)(.*)(&token_type=)(.*)(&state=)(.*)/;
@@ -28,27 +39,85 @@ const Home = () => {
     window.history.replaceState(null, '', homeUrl);
     if (results) {
       const accessToken = results[2];
-      localStorage.setItem('accessToken', accessToken)
+      localStorage.setItem('accessToken', accessToken);
       const expiresIn = results[4];
       const incomingState = results[8];
       const stateOk = incomingState === localStorage.getItem('token');
-      !stateOk ? console.log('please login again') : console.log('okay!!!')
+      !stateOk ? console.log('please login again') : console.log('okay!!!');
     }
   }
+  const sendEnvelope = () => {
+    console.log('send envelope...');
+    const requestBody = {
+      emailSubject: 'Please sign the attached document',
+      status: 'sent',
+      recipients: {
+        signers: [
+          {
+            email: email,
+            name: name,
+            recipientId: '1',
+          },
+        ],
+      },
+      documents: [
+        {
+          name: 'test',
+          fileExtension: 'pdf',
+          documentId: '1',
+          documentBase64: fileURL,
+        },
+      ],
+    };
+    console.log(requestBody)
+    const url=`${process.env.REACT_APP_DS_BASE_URI}/restapi/v2.1/accounts/${process.env.REACT_APP_DS_ACCOUNT_ID}/envelopes`
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    }).then((res) => console.log(res.json()))
+  };
   return (
     <div className='App container'>
       <h1>docuSign demo app</h1>
+      <h3>remote (email) signing</h3>
       <br />
       <button onClick={() => logout()}>Logout</button>
       <br />
-      <p>Send an envelope with a remote (email) signer</p>
-      Signer's email id: <input type='text' />
       <br />
-      <br />
-      Signer's name: <input type='text' />
-      <br />
-      <br />
-      <button>Send envelope</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendEnvelope();
+        }}
+      >
+        Attach agreement:{' '}
+        <input
+          required
+          type='file'
+          accept='application/pdf'
+          onChange={(e) => toBase64(e.target.files[0])}
+        />
+        <br />
+        <br />
+        Signer's email id:{' '}
+        <input
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+        />
+        <br />
+        <br />
+        Signer's name:{' '}
+        <input required onChange={(e) => setName(e.target.value)} type='text' />
+        <br />
+        <br />
+        <button>Send envelope</button>
+      </form>
     </div>
   );
 };
